85b328faff6cb80471993b0ebd8714115c255fce
Michael Diolosa <michael.diolosa@gmail.com>
2009-12-21 00:06:55 -0500

    Added the display of the configured platform within the popup.
    Fixed some more minor CSS bugs.
    
    Moved from the use of a <pre> tag to a regular div.  All double new lines have been replaced with two <br /> tags.

--- src/javascript/mbrio/SnapshotPopup.js
+++ src/javascript/mbrio/SnapshotPopup.js
@@ -20,6 +20,7 @@ mbrio.SnapshotPopup = function() {
 	this.changeLog_ = null;
 	this.revision_ = null;
 	this.href_ = null;
+	this.platform_ = null;
 	
 	this.init();
 }
@@ -32,13 +33,16 @@ mbrio.SnapshotPopup.prototype.retrieveChangeLog = function() {
 	var bgpage = chrome.extension.getBackgroundPage();
 	
 	this.changeLog_ = bgpage.snapshot.changeLogMessage;
+	if (this.changeLog_ != null && this.changeLog_.length > 0) this.changeLog_ = this.changeLog_.replace(/\n\n/g, "<br /><br />");
+	
 	this.revision_ = bgpage.snapshot.changeLogRevision;
 	this.href_ = bgpage.snapshot.downloadLink;
+	this.platform_ = bgpage.snapshot.platform;
 	this.display();
 }
 
 mbrio.SnapshotPopup.prototype.display = function() {
 	var panel = goog.dom.$dom('div');
-	panel.innerHTML = mbrio.t.Popup.changeLog({href:this.href_, revision:this.revision_, msg:this.changeLog_});
+	panel.innerHTML = mbrio.t.Popup.changeLog({href:this.href_, revision:this.revision_, msg:this.changeLog_, platform:this.platform_});
 	goog.dom.appendChild(document.body, panel);
 }
